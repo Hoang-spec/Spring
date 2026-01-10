38c10d9ce4b8f511996a10f08d6901e5277cd0ef
Hoang-spec <hoang106431@gmail.com>
2026-01-10 11:48:26 +0700

    Fisrt - Commit

--- src/main/java/com/example/schoolmanager/model/Student.java
+++ src/main/java/com/example/schoolmanager/model/Student.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "students")
+@Table(name = "schoolbale")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
